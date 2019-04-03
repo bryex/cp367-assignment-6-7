f98a6943d7b773153e1810f252c4c0acf959cbea
Mike Hoang <mike.hoang17@gmail.com>
2019-04-03 10:04:21 -0400

    question 1a

---
+++ listallthethings.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Task 1 - listallthings.sh
+# -------------------------
+
+echo "Starting listallthings.sh..."
+echo
+
+file_arr=( "$@" )
+
+# Loop all the arguments
+for file in "${file_arr[@]}"
+do
+    echo "file: $file"
+   
+    # Check if file is in current directory or the one below
+    if [ -f $file ]
+    then 
+        file_found=true 
+    else 
+        cd ../
+        if [ -f $file ]
+        then
+            file_found=true
+        fi
+    fi
+    
+    # List the file
+    if [ '$file_found = true' ]
+    then 
+        ls -l $file
+    else
+        echo "$file doesn't exists in the current directory or the on beneath."
+    fi
+
+    # Reset flags
+    file_found=false
+    cd "$OLDPWD" 
+    echo
+
+done
+
+echo "...ending listallthings.sh"
+
