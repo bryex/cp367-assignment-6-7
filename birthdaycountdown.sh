d0e338a1655083e5d1ad2a6e83d3556aa5732de3
bryex <43212311+bryex@users.noreply.github.com>
2019-04-04 23:12:11 -0400

    Special dates
    included an if statement which will give a special message if its the right day

--- birthdaycountdown.sh
+++ birthdaycountdown.sh
@@ -49,6 +49,32 @@ birth_date=$(date -j -f "%d-%m-%y" $b_day-$b_month-$year +%s)
 (( epoch_days_until_birthday = birth_date - current_date ))
 days_until_birthday=$((epoch_days_until_birthday/SEC_IN_DAY))
 
+#Special Dates
+if (($curr_month == 12))
+then
+    if (($curr_day == 25))
+    then
+	echo "Merry Christmas"
+    fi
+elif ((curr_month == 10))
+then
+    if (($curr_day == 31))
+    then
+	echo "Happy Halloween"
+    fi
+elif ((curr_month == 7))
+then
+    if (($curr_day == 1))
+    then
+	echo "Happy Canada Day"
+    fi
+elif ((curr_month == 3))
+then
+    if (($curr_day == 17))
+    then
+	echo "Happy St. Patricks Day"
+    fi
+fi
 
 # Print output
 if (( $days_until_birthday == 0 ))
